e3500c298923e1120aeebe7e8076dfe29132adee
Alexandro Fuentelzas <alex271@live.com>
2020-12-01 20:30:36 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [one, setOne] = useState("")
+  const [two, setTwo] = useState("")
+  const [three, setThree] = useState("")
+  const [separator, setSeparator]  = useState("");
+  const [concString, setConcatString] = useState("");
+
+  function concatinateStrings(){
+    let finalSeparator = separator == "" ? "" : separator + " ";
+    setConcatString(one + finalSeparator + two + finalSeparator + three)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="instructions">
+        Instructions: Enter text in each input box below. <br/>
+        The number represents the sequential order that will <br/>
+        be displayed in the output box. <br/>
+        The S stands for Seperator, and by default will be empty. <br/>
+        E.g 1 = "A", 2 = "B", 3 = "C", Output = "ABC" <br/>
+        Add a seperater value to seperate each of the strings. <br/>
+        E.g 1 = "A", 2 = "B", 3 = "C", S = ",", Output = "A, B, C"
+
+      </div>
+      <div className="userInputBlock">
+        Enter 3 strings below
+        <div className="userInput"> 1 <input onChange={ event => setOne(event.target.value)} style={{width:"75%"}} placeholder="Add first value here"></input> </div> 
+        <div className="userInput"> 2 <input onChange={ event => setTwo(event.target.value)} style={{width:"75%"} } placeholder="Add second value here"></input> </div> 
+        <div className="userInput"> 3 <input onChange={ event => setThree(event.target.value)} style={{width:"75%"} } placeholder="Add third value here"></input> </div> 
+        <div className="userInput"> S <input onChange={ event => setSeparator(event.target.value)}style={{width:"75%"} } placeholder="Add seperator value here"></input> </div>
+        <button onClick={concatinateStrings} className="submitButton"> Submit </button> 
+      </div>
+
+      <div className="outputBlock">
+        <div style={{fontSize:"20px"}}> Output </div>
+        {concString == "" ? "String will be displayed here" : concString}
+
+      </div>
+
     </div>
+    
   );
 }
 
